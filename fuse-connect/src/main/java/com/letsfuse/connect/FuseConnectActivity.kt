4b8a9e1489eb22424f16c55aae721a17eabf17a6
Tunde Alao <46252854+TundeAlao@users.noreply.github.com>
2023-01-13 14:53:37 +0100

    fuse connect activity

---
+++ fuse-connect/src/main/java/com/letsfuse/connect/FuseConnectActivity.kt
@@ -0,0 +1,42 @@
+package com.letsfuse.connect
+import android.net.Uri
+import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class FuseConnectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fuse_connect)
+
+        val webView = findViewById<WebView>(R.id.webview)
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                return if (request != null) {
+                    handleUri(request.url)
+                } else {
+                    true
+                }
+            }
+
+            @Deprecated("Deprecated in Java")
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                return handleUri(Uri.parse(url))
+            }
+        }
+        webView.loadUrl("https://www.example.com")
+    }
+
+    private fun handleUri( uri: Uri): Boolean {
+        if (uri.scheme == "fuse:") {
+            // Handle URL scheme here
+            return true
+        }
+        return false
+    }
+}
