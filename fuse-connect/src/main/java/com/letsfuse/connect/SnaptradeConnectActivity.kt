b48b36a3162c37c5e3afac1907e4ec4a402dd524
AdibFuse <106388522+AdibFuse@users.noreply.github.com>
2024-01-03 15:21:26 +0000

    Fallback aggregator plaid + custom search UI (#10)

--- fuse-connect/src/main/java/com/letsfuse/connect/SnaptradeConnectActivity.kt
+++ fuse-connect/src/main/java/com/letsfuse/connect/SnaptradeConnectActivity.kt
@@ -24,6 +24,7 @@ class SnaptradeConnectActivity : Activity() {
                         finish()
                     }
                     it.contains("ABANDONED") -> {
+                        onExit!!()
                         finish()
                     }
                     it.contains("ERROR") -> {
